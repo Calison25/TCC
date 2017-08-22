f9c4bb3a6bcd2fb90fa7a1003baf54bb0017b845
Calison <calisonsi@gmail.com>
2017-08-22 10:21:02 -0300

    Busca os ids de busca

--- Classes/GenBankCrawler.php
+++ Classes/GenBankCrawler.php
@@ -6,15 +6,28 @@ use GuzzleHttp\Cookie\CookieJar;
 
 class GenBankCrawler
 {
+	
+	const DEFAULT_URL = 'https://www.ncbi.nlm.nih.gov/';
 
 	public function __construct()
 	{
 		$cookie = new CookieJar();
+		$ids = [];
 
         // instancia pro client
         $client = new Client(['cookies' => $cookie, 'verify' => false]);
 
-        $response = $client->get('https://www.google.com.br/?gws_rd=cr&ei=UPiYWc6JIIqxwASN-KygDA');
+        $client->get(self::DEFAULT_URL.'genbank/');
+
+        $response = $client->get(self::DEFAULT_URL.'genome/?term=dengue');
+
+        $content = strip_tags($response->getBody()->getContents());
+
+        preg_match_all('/ID: \d+/', $content, $matches);
+
+        $ids = array_map(function($match){
+        	return trim(str_replace('ID:', '', $match));
+        }, $matches[0]);
 	}
 }
 
