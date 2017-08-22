fbd2365c7b47bd77e3bd83f6dab92b222b27eb19
Calison <calisonsi@gmail.com>
2017-08-22 10:55:12 -0300

    Salva o arquivo como pdf

--- Classes/GenBankCrawler.php
+++ Classes/GenBankCrawler.php
@@ -19,7 +19,7 @@ class GenBankCrawler
 
         $client->get(self::DEFAULT_URL.'genbank/');
 
-        $response = $client->get(self::DEFAULT_URL.'genome/?term=dengue');
+        $response = $client->get(self::DEFAULT_URL.'pubmed/?term=dengue');
 
         $content = strip_tags($response->getBody()->getContents());
 
@@ -28,6 +28,14 @@ class GenBankCrawler
         $ids = array_map(function($match){
         	return trim(str_replace('ID:', '', $match));
         }, $matches[0]);
+
+        $client->get(self::DEFAULT_URL.'pubmed/'.$ids[0]);
+
+        $client->get('http://journals.sagepub.com/doi/10.1177/0022034517723325');
+
+        $response = $client->get('http://journals.sagepub.com/doi/pdf/10.1177/0022034517723325');
+
+        file_put_contents('teste.pdf', $response->getBody()->getContents());
 	}
 }
 
